cc575a4bbed1fc78cd6ca84e62cb45811f586540
gentlebreeza <Jason@Jason-Lis-MacBook-Air.local>
2016-06-11 21:12:36 -0700

    move file from bin to lib

---
+++ destinations/src/lib/destinations.rb
@@ -0,0 +1,31 @@
+require 'oj'
+require 'ap'
+
+class Destinations
+
+    destinations = {
+        "Lake Como"                           =>     ["Italy", "the place where 'Star Wars' and '007 Casino Royale' shot the scene."],
+        "Methoni Castle"                      =>     ["Greece"],
+        "Tahiti"                              =>     [],
+        "Salar de Uyuni"                      =>     ["Bolivia"],
+        "Siena"                               =>     ["Italy"],
+        "Sicily"                              =>     ["Italy"],
+        "Verona"                              =>     ["Italy"],
+        "Alberobello"                         =>     ["Italy", "Trullo, mushroom like house"],
+        "Matera"                              =>     ["Italy"],
+        "Talamone"                            =>     ["Italy"],
+        "Parco Nazionale delle Cinque Terre"  =>     ["Italy", "wu yu cun, Cinque Terre"],
+        "Amalfi Coast"                        =>     ["Italy", "near Cinque Terre"],
+        "Villa la Gaeta"                      =>     ["Italy", "the place where '007 Casino Royale' shot the scene", "where Mr White died in the movie"],
+        "Villa del Balbianello"               =>     ["Italy", "the place where '007 Casino Royale' shot the scene", "where James Bond took recuperation"],
+        "Guo Shang Martyrs' Cemetery"         =>     ["China", "Teng Chong"],
+    }
+    
+    def get_destinations
+        destinations
+    end
+    
+    def get_json_destinations
+        Oj.dump destinations
+    end
+end
